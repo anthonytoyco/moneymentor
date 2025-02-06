350263e629fd6dea119a2d649d4df9ab9015f6a2
Anthony Toyco <amieltoyco@gmail.com>
2025-02-06 02:05:01 +0000

    testing auto deploy

--- app/main.py
+++ app/main.py
@@ -6,5 +6,6 @@ quotes = Quotes()
 ui.button("Quote", on_click=lambda: ui.notify(quotes.random()[1]))
 
 ui.label("Test Label")
+ui.label("Test Label")
 
 ui.run(reload="FLY_ALLOC_ID" not in os.environ)
