976b4fb89b896b200ecb6311e7ab62d2e3f60fe0
Anthony Toyco <amieltoyco@gmail.com>
2025-02-07 04:54:15 +0000

    initialized fastapi and supabase

--- api/main.py
+++ api/main.py
@@ -1,15 +1,17 @@
-from typing import Union
-
+from supabase import Client, create_client
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+
+# Initialize Supabase client
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
